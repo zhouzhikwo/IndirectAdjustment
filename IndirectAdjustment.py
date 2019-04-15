1abf3a11b847432084e621a40a5a421f87f1760c
zhouzhikwo <zhouzhikwo@outlook.com>
2019-04-15 21:57:39 +0800

    完善以提高可用性

--- IndirectAdjustment.py
+++ IndirectAdjustment.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 
-# 基线数据
-line = [[0 for i in range(6)] for j in range(24)]
+# 读取基线数据
 line = np.loadtxt("source.txt")
 
 # 点位数据，将P1(0, 0, 0)置为已知点
-point = [[0 for i in range(3)] for j in range(14)]
+PointNumber=int(max(line[:,2].max(),line[:,1].max()))
+point = [[0 for i in range(3)] for j in range(PointNumber)]
 point[0] = [0, 0, 0]
-for i in range(1, 14, 1):
-    for j in range(0, 24, 1):
+for i in range(1, PointNumber, 1):
+    for j in range(0, line.shape[0], 1):
         if line[j][2] == i + 1 and line[j][1] <= i:
             point[i] = [
                 x + y for x, y in zip(point[int(line[j][1] - 1)], line[j][3:])
@@ -21,19 +21,19 @@ for i in range(1, 14, 1):
             ]
 
 # 从基线数据中提取系数构造B矩阵，将基线中起点位置对应的系数置为-1，终点位置对应的系数置为1
-matrix_B = [[0 for i in range(14)] for j in range(24)]
-for i in range(0, 24):
+matrix_B = [[0 for i in range(PointNumber)] for j in range(line.shape[0])]
+for i in range(0, line.shape[0]):
     matrix_B[i][int(line[i][2]) - 1] = 1
     matrix_B[i][int(line[i][1]) - 1] = -1
 
 # 提取基线数据中的x, y, z增量，方便后续计算
-line_data = [line[iter][3:6] for iter in range(0, 24)]
+line_data = [line[iter][3:6] for iter in range(0, line.shape[0])]
 
 # 计算误差方程常数项
 l = line_data - np.dot(np.array(matrix_B), np.array(point))
 
 # 等权观测，构造单位对角阵
-matrix_P = np.eye(24)
+matrix_P = np.eye(line.shape[0])
 
 # 计算法方程的解x
 Nbb = np.dot(np.dot(np.transpose(matrix_B), matrix_P), matrix_B)
